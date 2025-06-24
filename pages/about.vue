42356311667f5eb442b58cb88878a672302883cf
pRom3o <alameleromeo1@gmail.com>
2025-06-24 10:40:21 +0100

    home, projects done. about left

--- pages/about.vue
+++ pages/about.vue
@@ -1,7 +1,7 @@
 <script setup></script>
 
 <template>
-  <div class="h-screen w-full flex items-center justify-center">
-    <h1 class="text-6xl font-bold">OMO NOTHING YET</h1>
+  <div class="h-screen w-full flex items-center justify-center p-8">
+    <h1 class="md:text-6xl text-3xl font-bold">OMO NOTHING YET</h1>
   </div>
 </template>
